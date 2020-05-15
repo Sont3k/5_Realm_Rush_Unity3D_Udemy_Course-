c53c15101d71e77d71b9caea0a098bcd7765fb79
Sont3k <markus.residence@gmail.com>
2020-05-15 16:27:10 +0300

    146. Check For Distance

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -10,7 +10,7 @@ public class EnemyDamage : MonoBehaviour
     private void OnParticleCollision(GameObject other) {
         ProcessHit();
 
-        if(hitPoints <= 1)
+        if(hitPoints <= 0)
         {
             KillEnemy();
         }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,22 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan, targetEnemy;
+    [SerializeField] float attackRange = 10f;
+    [SerializeField] ParticleSystem projectileParticle;
 
-    // Update is called once per frame
     void Update()
     {
         if(targetEnemy)
         {
             LookAtEnemy();
+            FireAtEnemy();
+        }
+        else
+        {
+            Shoot(false); //TODO producing null error
         }
     }
 
     public void LookAtEnemy()
     {
         objectToPan.LookAt(targetEnemy);
+    }
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.position, transform.position);
+        
+        if(distanceToEnemy <= attackRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
