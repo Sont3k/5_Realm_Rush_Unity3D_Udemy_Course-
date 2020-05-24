709d540fb223735abc7345cf84e0e15de10ff0b9
Sont3k <markus.residence@gmail.com>
2020-05-24 14:00:15 +0300

    155. Implementing A Ring Buffer

---
+++ Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour
+{
+    [SerializeField] int towerLimit = 5;
+    [SerializeField] Tower tower;
+    // [SerializeField] GameObject towerHolder;
+
+    int numTowers = 0;
+
+    public void AddTower(Waypoint baseWaypoint)
+    {
+        if (numTowers < towerLimit)
+        {
+            InstantiateNewTower(baseWaypoint);
+        }
+        else
+        {
+            MoveExistingTower();
+        }
+    }
+
+    private static void MoveExistingTower()
+    {
+        print("Can't place new tower, tower limit is reached.");
+        //todo actually moving
+    }
+
+    private void InstantiateNewTower(Waypoint baseWaypoint)
+    {
+        tower.transform.position = new Vector3(baseWaypoint.transform.position.x, baseWaypoint.transform.position.y - 2.5f, baseWaypoint.transform.position.z);
+
+        var towerClone = Instantiate(tower, tower.transform.position, transform.rotation);
+        // towerClone.transform.parent = towerHolder.transform;
+        baseWaypoint.isPlaceable = false;
+
+        numTowers++;
+    }
+}
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -9,9 +9,6 @@ public class Waypoint : MonoBehaviour
     public Waypoint exploredFrom;
     public bool isPlaceable = true;
 
-    [SerializeField] Tower tower;
-    // [SerializeField] GameObject towerHolder;
-
     Vector2Int gridPos;
     const int gridSize = 10;
 
@@ -36,17 +33,11 @@ public class Waypoint : MonoBehaviour
 
     private void OnMouseOver()
     {
-        tower.transform.position = new Vector3(transform.position.x, transform.position.y - 2.5f, transform.position.z);
-
         if (Input.GetMouseButtonDown(0)) // left click
         {
             if (isPlaceable)
             {
-                print(gameObject.name + " tower placement");
-                var towerClone = Instantiate(tower, tower.transform.position, transform.rotation);
-                
-                // towerClone.transform.parent = towerHolder.transform;
-                isPlaceable = false;
+                FindObjectOfType<TowerFactory>().AddTower(this);
             }
             else
             {
