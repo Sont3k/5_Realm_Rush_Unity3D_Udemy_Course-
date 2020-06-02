e19d2b48dd4af0fbacc761d6c7f88c36e527d0da
Sont3k <markus.residence@gmail.com>
2020-06-02 21:54:15 +0300

    160. Display Health & Score

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     [Range(.1f, 120f)][SerializeField] float secondsBetweenSpawns = 2f;
     [SerializeField] EnemyMovement enemyPrefab;
     [SerializeField] Transform enemiesParent;
+    [SerializeField] Text spawnedEnemies;
+    int score;
 
     void Start()
     {
         StartCoroutine(RepeatedlySpawnEnemies());
+        SetScoreText();
     }
 
     IEnumerator RepeatedlySpawnEnemies()
@@ -19,8 +23,15 @@ public class EnemySpawner : MonoBehaviour
         {
             var enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             enemy.transform.parent = enemiesParent;
+            score++;
+            SetScoreText();
 
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    private void SetScoreText()
+    {
+        spawnedEnemies.text = "Score: " + score.ToString();
     }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int health = 10;
     [SerializeField] int healthDecrease = 1;
+    [SerializeField] Text healthText;
+
+    private void Start() {
+        SetHealthText();
+    }
 
     private void OnTriggerEnter(Collider other) {
         health -= healthDecrease;
+        SetHealthText();
+    }
+
+    private void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
     }
 }
